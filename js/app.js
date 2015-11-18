6e8cf75d1f1e65d1a018a2ec25a02036d2a3b615
Tyler Schmidt <thedoorkeep@gmail.com>
2015-11-18 10:13:44 -0600

    whooooole lotta stuff

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+function ShowDiv() {
+    document.getElementById("Billing").style.display = "none";
+}
